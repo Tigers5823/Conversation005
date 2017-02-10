46566ef3c1e280606e4321d15ace5193dbf0bc82
文崇 木場 <fu-koba@city.co.jp>
2017-02-10 02:57:05 -0600

    test001

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -8,6 +8,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.ibm.watson.developer_cloud.conversation.v1.ConversationService;
+import com.ibm.watson.developer_cloud.conversation.v1.model.MessageRequest;
+import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
+
+
 /**
  * Servlet implementation class SimpleServlet
  */
@@ -22,6 +27,22 @@ public class SimpleServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         response.getWriter().print("Hello World!");
-    }
 
+        ConversationService service = new ConversationService("2016-09-20");
+        service.setUsernameAndPassword("f45c965e-919d-4f8a-a9e9-54a62512837f", "dVg7kHTD4SsH");
+
+        MessageRequest newMessage = new MessageRequest.Builder()
+        .inputText("Turn on the lights")
+        // Replace with the context obtained from the initial request
+        //.context(...)
+        .build();
+
+		String workspaceId = "ebd46f71-488a-411a-a302-1ba865db9e61";
+
+		MessageResponse response1 = service
+ 		 .message(workspaceId, newMessage)
+ 		 .execute();
+
+		System.out.println(response1);
+    }
 }
